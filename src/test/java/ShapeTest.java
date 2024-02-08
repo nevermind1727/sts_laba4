97c563aa1f673c8c5f93b655c6ded94de77575eb
Dmitry Taran <dmit.taran2017@gmail.com>
2024-02-08 13:53:49 +0200

    test color changed

--- src/test/java/ShapeTest.java
+++ src/test/java/ShapeTest.java
@@ -7,7 +7,7 @@ public class ShapeTest {
     @Test
     void ShapeByNumberTest(){
         ShapeFactory shape1 = new ShapeFactory(18);
-        Assertions.assertEquals(Color.BLACK,shape1.paint);
+        Assertions.assertEquals(Color.RED,shape1.paint);
         ShapeFactory shape2 = new ShapeFactory(77);
         Assertions.assertEquals(GradientPaint.class, shape2.paint.getClass());
     }
